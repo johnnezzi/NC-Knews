a9381a87b9d3bad4c4897b451cfcb58143577b99
John Nezzi <john@Johns-MacBook-Pro.local>
2019-02-11 10:18:59 +0000

    testing

--- db/migrations/20190114113327_create_comments_table.js
+++ db/migrations/20190114113327_create_comments_table.js
@@ -3,7 +3,7 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTable('comments', (table) => {
     table.increments('comments_id').primary().notNullable();
     table.string('username').references('users.username').notNullable();
-    table.integer('article_id').references('articles.article_id');
+    table.integer('article_id').references('articles.article_id').cascade("DELETE");
     table.integer('votes').defaultsTo(0).notNullable();
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.string('body', 2000).notNullable();
